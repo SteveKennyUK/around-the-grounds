bb62df975469aaaa69965108df55b89d7630f164
SteveKennyUK <steven.kenny@hotmail.co.uk>
2021-07-19 10:24:03 +0000

    change zoom and height of map so it displays

--- assets/js/maps.js
+++ assets/js/maps.js
@@ -3,15 +3,13 @@
   <script async defer src="https://maps.googleapis.com/maps/api/js?key=AIzaSyA_056RXCG-BAcN58URaksq8TBBk4M4s4w&callback=initMap"></script>
    Credit: Guidance provided by Code Institute tutorials and Google Maps Platform - https://developers.google.com/maps/documentation/javascript/overview */
 
-   function initMap() {
+function initMap() {
     // Set initial map properties
     const mapProp = {
         center: new google.maps.LatLng(51.509, -0.122),
-        zoom: 8,
+        zoom: 9,
         scrollwheel: true
     };
 
     const map = new google.maps.Map(document.getElementById("map"), mapProp);
 } 
-
-
