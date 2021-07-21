a20882040bb6486464ac0c372fecacc4a4d51d6a
SteveKennyUK <steven.kenny@hotmail.co.uk>
2021-07-21 23:45:11 +0000

    add logo change to marker click event

--- assets/js/maps.js
+++ assets/js/maps.js
@@ -20,48 +20,56 @@
             lng: -0.10835,
             name: "Emirates Stadium",
             club: "Arsenal",
+            logo: "assets/images/stadiums/arsenal-crest.jpg",
         },
         {
             lat: 51.49089,
             lng: -0.28908,
             name: "Brentford Community Stadium",
             club: "Brentford",
+            logo: "assets/images/stadiums/brentford-crest.jpg",
         },
         {
             lat: 51.48177,
             lng: -0.19042,
             name: "Stamford Bridge",
             club: "Chelsea",
+            logo: "assets/images/stadiums/chelsea-crest.jpg",
         },
         {
             lat: 51.39854,
             lng: -0.08533,
             name: "Selhurst Park",
             club: "Crystal Palace",
+            logo: "assets/images/stadiums/palace-crest.jpg",
         },
         {
             lat: 51.60431,
             lng: -0.06638,
             name: "Tottenham Hotspur Stadium",
             club: "Tottenham Hotspur",
+            logo: "assets/images/stadiums/spurs-crest.jpg",
         },
         {
             lat: 51.65003,
             lng: -0.40203,
             name: "Vicarage Road",
             club: "Watford",
+            logo: "assets/images/stadiums/watford-crest.jpg",
         },
         {
             lat: 51.53892,
             lng: -0.01625,
             name: "London Stadium",
             club: "West Ham United",
+            logo: "assets/images/stadiums/west-ham-crest.jpg",
         },
         {
             lat: 51.55621,
             lng: -0.27926,
             name: "Wembley Stadium",
             club: "National Stadium",
+            logo: "assets/images/stadiums/wembley-logo.png",
         },
     ];
 
@@ -105,7 +113,8 @@
             "click",
             (function (i) {
                 return function () {
-                    document.getElementById("card-title").innerHTML = stadiums[i].club;
+                    document.getElementById("card-title").textContent = stadiums[i].club;
+                    document.getElementById("crest-card").innerHTML = `<img class="card-img-top mt-5" src="${stadiums[i].logo}" alt="${stadiums[i].club} logo">`;
                 };
             })(i)
         );
