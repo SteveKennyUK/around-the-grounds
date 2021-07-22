308db231ff06a4cadbca9fa463aad8f86c0b5700
SteveKennyUK <steven.kenny@hotmail.co.uk>
2021-07-22 21:16:57 +0000

    add (document).ready(initMap); to maps.js

--- assets/js/maps.js
+++ assets/js/maps.js
@@ -2,6 +2,7 @@
    Create initMap() function to be invoked once the following JavaScript file has been loaded in index.html:
   <script async defer src="https://maps.googleapis.com/maps/api/js?key=AIzaSyA_056RXCG-BAcN58URaksq8TBBk4M4s4w&callback=initMap"></script>
    Credit: Guidance provided by Code Institute tutorials, Eamonn Smyth (see Credits section in README) and Google Maps Platform - https://developers.google.com/maps/documentation/javascript/overview */
+   $(document).ready(initMap)
 
    function initMap() {
     // Set initial map properties
@@ -223,3 +224,4 @@
         }
     }
 }
+
