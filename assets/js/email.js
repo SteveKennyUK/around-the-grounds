b5bac19b4c25f5c8d418de1ce479e311f7faf077
SteveKennyUK <steven.kenny@hotmail.co.uk>
2021-07-23 23:57:47 +0000

    add modal, called when email sent successfully

--- assets/js/email.js
+++ assets/js/email.js
@@ -1,6 +1,7 @@
 /* jshint esversion: 6, jquery: true */
 /* Guidance on setting up the email service provided by EmailJS - https://www.emailjs.com/ 
    Includes sample testing code at https://jsfiddle.net/api/post/library/pure/ */
+/* Credit: Stack Overflow for opening a modal using jQuery - https://stackoverflow.com/questions/13183630/how-to-open-a-bootstrap-modal-window-using-jquery */   
 
 const btn = document.getElementById("button");
 const contactForm = document.getElementById("form");
@@ -15,7 +16,8 @@ form.addEventListener('submit', function(event) {
 
    emailjs.sendForm(serviceID, templateID, this)
     .then(function(response) {
-        console.log("Success", response.status, response.text);    
+        console.log("Success", response.status, response.text);  
+        $('#email-success').modal('show');  
         btn.value = "Send";
         resetForm();
     }, function(error) {
