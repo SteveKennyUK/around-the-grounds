c2e004eb68fb516105ca25e2d4f0b2faad4d355b
SteveKennyUK <steven.kenny@hotmail.co.uk>
2021-07-23 22:49:02 +0000

    clear form fields after sending; resize send button

--- assets/js/email.js
+++ assets/js/email.js
@@ -1,25 +1,36 @@
 /* jshint esversion: 6, jquery: true */
-/* Guidance on setting up the email service provided by EmailJS - https://www.emailjs.com/ */
+/* Guidance on setting up the email service provided by EmailJS - https://www.emailjs.com/ 
+   Includes sample testing code at https://jsfiddle.net/api/post/library/pure/ */
 
-const btn = document.getElementById('button');
+const btn = document.getElementById("button");
+const contactForm = document.getElementById("form");
 
-document.getElementById('form')
- .addEventListener('submit', function(event) {
-   event.preventDefault();
+form.addEventListener('submit', function(event) {
+   event.preventDefault(); // prevent reload
 
-   btn.value = 'Sending...';
+   btn.value = "Sending...";
 
-   const serviceID = 'gmail';
-   const templateID = 'around-the-grounds';
+   const serviceID = "gmail";
+   const templateID = "around-the-grounds";
 
    emailjs.sendForm(serviceID, templateID, this)
-    .then(() => {
-      btn.value = 'Send';
-      alert('Sent!');
-    }, (err) => {
-      btn.value = 'Send';
-      alert(JSON.stringify(err));
+    .then(function(response) {
+        console.log("Success", response.status, response.text);    
+        btn.value = "Send";
+        resetForm();
+    }, function(error) {
+        console.log(JSON.stringify(error));
+        btn.value = "Send";
+      
     });
+
+    return false; // prevents the page from refreshing when the form is submitted
 });
-
-
+// Reset the form fields to original default values
+function resetForm() {
+    document.getElementById("first-name").value = "";
+    document.getElementById("last-name").value = "";
+    document.getElementById("email-address").value = "";
+    document.getElementById("tours").value = "Which stadium/tour are you interested in?";
+    document.getElementById("message").value = "";
+}
